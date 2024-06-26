5e83a81c8d716f777d70734a8837e2f1e0717af3
Webseite Für All Ihre Probleme <johann.krantz@fragy.de>
2024-06-26 19:24:04 +0200

    Update script_weather.js

--- script_weather.js
+++ script_weather.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "af1947fe31f1a7afaf4a4c726745d4a0";
+const apiKey = "";
 
 weatherForm .addEventListener("submit", async event => {
     event.preventDefault();
